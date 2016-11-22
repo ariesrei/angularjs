14799bb73a7f32281fb40af0c860aebf6c5c54ef
ariesrei <arsrymeralles@gmail.com>
2016-11-22 15:14:26 +0800

    1

---
+++ dist/angular-test.js
@@ -0,0 +1,27 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+    if (typeof exports === "object" && typeof module === "object") module.exports = factory(); else if (typeof define === "function" && define.amd) define("angular-test", [], factory); else if (typeof exports === "object") exports["angular-test"] = factory(); else root["angular-test"] = factory();
+})(this, function() {
+    return function(modules) {
+        var installedModules = {};
+        function __webpack_require__(moduleId) {
+            if (installedModules[moduleId]) return installedModules[moduleId].exports;
+            var module = installedModules[moduleId] = {
+                exports: {},
+                id: moduleId,
+                loaded: false
+            };
+            modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+            module.loaded = true;
+            return module.exports;
+        }
+        __webpack_require__.m = modules;
+        __webpack_require__.c = installedModules;
+        __webpack_require__.p = "/assets/";
+        return __webpack_require__(0);
+    }([ function(module, exports, __webpack_require__) {
+        module.exports = __webpack_require__(1);
+    }, function(module, exports, __webpack_require__) {
+        __webpack_require__(2);
+    }, function(module, exports) {} ]);
+});
+
